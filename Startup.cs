4e0cdaac21d823a98cf57ec60f77cb916193d4f7
navule <pavanraonavule@gmail.com>
2021-01-16 01:24:37 +0530

    init 🚀

---
+++ Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ElectronNET.API;
+
+namespace tutlinks_electron_hello_world
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+            if(HybridSupport.IsElectronActive){
+                CreateElectronWindow();
+            }
+        }
+
+        private async void CreateElectronWindow(){
+            var window = await Electron.WindowManager.CreateWindowAsync();
+            window.OnClosed += Window_OnClosed; 
+            window.OnMaximize += Window_OnMaximize;
+        }
+
+        private void Window_OnClosed(){
+            Electron.App.Exit();
+        }
+
+        private void Window_OnMaximize(){
+            Electron.Dialog.ShowErrorBox("Demo Maximized event", "Hi, You just maximized your Electron App's Window");
+        }
+    }
+}
